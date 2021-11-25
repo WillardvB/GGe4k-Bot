0e3e402b04fc44b5a051cdd877eb010ec58d7c73
vanBrusselGames <wlvb01@gmail.com>
2021-11-25 21:30:23 +0100

    v0.0.1

--- website/site.js
+++ website/site.js
@@ -9,7 +9,8 @@ module.exports = {
 
 function publishSite()
 {
-  app.get('/', (req, res) => {
-    res.send('<head><meta name="google-site-verification" content="FKrV3op8mG8X8NSJwuc-uv--9a0jiG8x3gmoYYhS0yY" /></head><body>We zijn online!</body>');
-  });
+    app.get('/', (req, res) => {
+        res.send('<head><meta name="google-site-verification" content="FKrV3op8mG8X8NSJwuc-uv--9a0jiG8x3gmoYYhS0yY" /></head><body>We zijn online!</body>');
+    });
+    app.listen();
 }
