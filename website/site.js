e73711fc91d79f92874a30f9d5ee2c6a607c908e
vanBrusselGames <wlvb01@gmail.com>
2021-11-25 21:26:24 +0100

    v0.0.1

--- website/site.js
+++ website/site.js
@@ -12,5 +12,8 @@ function publishSite()
   app.get('/', (req, res) => {
     res.send('<head><meta name="google-site-verification" content="FKrV3op8mG8X8NSJwuc-uv--9a0jiG8x3gmoYYhS0yY" /></head><body>We zijn online!</body>');
   });
-  app.listen();
+  const PORT = process.env.PORT || 6565;
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
 }
