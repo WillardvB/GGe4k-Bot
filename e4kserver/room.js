d068a66cda66fae3a088e645ab28185b1fc1146a
vanBrusselGames <88274071+vanBrusselGames@users.noreply.github.com>
2022-07-18 20:54:19 +0200

    Update room.js

--- e4kserver/room.js
+++ e4kserver/room.js
@@ -69,6 +69,10 @@ function _setRoomList(data) {
     console.log("cannot find onRoomListUpdate for roomData: " + JSON.stringify(roomData));
 }
 
+function getAllRooms() {
+    return roomList;
+}
+
 /**
  * 
  * @param {number} index
@@ -78,6 +82,29 @@ function _getRoom(index) {
         return null;
     }
     return roomList[index];
+}
+
+/**
+ * 
+ * @param {string} input
+ */
+function decompressInt(input) {
+    let _loc3_ = 0;
+    let _loc6_ = "A".charCodeAt(0);
+    let _loc5_ = "z".charCodeAt(0);
+    let _loc4_ = _loc5_ - _loc6_ + 1;
+    let _loc2_ = 0;
+    if (input && input.length > 0) {
+        _loc3_ = input.length - 1;
+        while (_loc3_ >= 0) {
+            if (input.charCodeAt(_loc3_) < _loc6_ || input.charCodeAt(_loc3_) > _loc5_) {
+                return -1;
+            }
+            _loc2_ = _loc2_ * _loc4_ + input.charCodeAt(_loc3_) - _loc6_;
+            _loc3_--;
+        }
+    }
+    return _loc2_;
 }
 
 /**
