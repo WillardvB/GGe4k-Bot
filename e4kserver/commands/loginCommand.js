e7b4c52b352a13ff262f41a0ed879f039e5a41ee
vanBrusselGames <88274071+vanBrusselGames@users.noreply.github.com>
2022-07-18 21:59:37 +0200

    update

--- e4kserver/commands/loginCommand.js
+++ e4kserver/commands/loginCommand.js
@@ -1,12 +1,13 @@
 module.exports = {
     name: "login",
     execute() {
+        let accountID = /*"165774755424378480";/*/Date.now().toString() + (Math.random() * 999999).toFixed();
         let CoreC2SLoginWithAuthenticationVO = {
             params: {
                 NM: "E4K-NL",
                 PW: "E4K-NL-server",
                 L: "nl",
-                AID: "165774755424378480",
+                AID: accountID,
                 DID: 5,
                 PLFID: "3",
                 ADID: "null",
@@ -15,6 +16,7 @@ module.exports = {
             },
             getCmdId: "core_lga"
         }
+        console.log("trying to login with parameters: " + JSON.stringify(CoreC2SLoginWithAuthenticationVO));
         require('./../data.js').sendJsonVoSignal({ "commandVO": CoreC2SLoginWithAuthenticationVO, "lockConditionVO": "new DefaultLockConditionVO()" });
     },
 }
--- e4kserver/onReceived/sys/logOK.js
+++ e4kserver/onReceived/sys/logOK.js
@@ -5,6 +5,6 @@ module.exports = {
         let _isMod = xml.body.login.$.mod;
         let _username = xml.body.login.$.n;
         console.log("Succesfully logged in! Username: " + _username);
-        getRoomList();
+        require('./../../room.js').getRoomList();
     }
 }
