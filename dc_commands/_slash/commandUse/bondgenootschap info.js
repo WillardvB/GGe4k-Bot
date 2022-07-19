93e59db9b7072e50f9b4b3f1d00afc7e64295204
vanBrusselGames <88274071+vanBrusselGames@users.noreply.github.com>
2022-07-19 15:57:24 +0200

    uipdate

--- dc_commands/_slash/commandUse/bondgenootschap info.js
+++ dc_commands/_slash/commandUse/bondgenootschap info.js
@@ -9,15 +9,19 @@ module.exports = {
      */
     async execute(interaction) {
         let allianceName = interaction.options.getString('naam');
+        console.log("allianceName: " + allianceName);
         let _alliances = require("../../e4kserver/data").alliances;
+        console.log("alliances Count: " + Object.keys(_alliances).length);
         let alliance = null;
         for (let allianceId in _alliances) {
             let _alliance = _alliances[allianceId];
+            console.log(_alliance.name + " is " + (_alliance.name == allianceName ? "wel" : "niet") + "hetzelfde ");
             if (_alliance.name == allianceName) {
                 alliance = _alliance;
             }
         }
-        if (alliance == null) return;
-        interaction.reply({content: "membercount: " + alliance.members.length})
+        console.log("found alliance:"  +alliance)
+        if (alliance == null) return interaction.reply({ content: "Sorry, ik heb het bg niet gevonden!", ephemeral = true });
+        interaction.reply({ content: "membercount: " + alliance.members.length, ephemeral = true })
     }
 }
