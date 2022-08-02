adc8db9c09774bdd67f1270e514d2319fc5bfdec
vanBrusselGames <88274071+vanBrusselGames@users.noreply.github.com>
2022-08-03 00:13:43 +0200

    Update ain.js

--- e4kserver/onReceived/xt/ain.js
+++ e4kserver/onReceived/xt/ain.js
@@ -1,4 +1,5 @@
 const myMongoDB = require('../../../myMongoDB.js');
+const Logger = require('../../../tools/Logger.js');
 const logger = require('../../../tools/Logger.js');
 
 let allianceId = 0;
@@ -99,58 +100,75 @@ function parseAllianceInfo(allianceInfo) {
  * @param {object} paramObject
  */
 function allianceInfoFillFromParamObject(paramObject) {
-    const wsp = require('./wsp.js');
-    let allianceInfoVO = {
-        allianceId: paramObject.AID,
-        allianceName: paramObject.N,
-        allianceDescription: parseChatJSONMessage(paramObject.D),
-        languageId: paramObject["ALL"],
-        memberLevel: paramObject.ML,
-        memberList: [],
-        allianceStatusToOwnAlliance: paramObject.DOA,
-        allianceFamePoints: paramObject.CF,
-        allianceFamePointsHighestReached: -1,
-        canInvitedForHardPact: paramObject.HP == 1,
-        canInvitedForSoftPact: paramObject.SP == 1,
-        isSearchingMembers: paramObject.IS,
-        landmarks: [],
-        isOpenAlliance: paramObject.IA != 0,
-        freeRenames: paramObject.FR,
-        might: parseInt(paramObject.MP),
-    }
-    let memberListArray = paramObject.M;
-    let _memberList = [];
-    let i = 0;
-    while (i < memberListArray.length) {
-        let member = wsp.parseOwnerInfo(memberListArray[i]);
-        if (memberListArray[i] && memberListArray[i].OID) {
-            member = {
-                playerId: memberListArray[i].OID,
-                allianceRank: parseInt(memberListArray[i].AR),
+    try {
+        const wsp = require('./wsp.js');
+        let allianceInfoVO = {
+            allianceId: paramObject.AID,
+            allianceName: paramObject.N,
+            allianceDescription: parseChatJSONMessage(paramObject.D),
+            languageId: paramObject["ALL"],
+            memberLevel: paramObject.ML,
+            memberList: [],
+            allianceStatusToOwnAlliance: paramObject.DOA,
+            allianceFamePoints: paramObject.CF,
+            allianceFamePointsHighestReached: -1,
+            canInvitedForHardPact: paramObject.HP == 1,
+            canInvitedForSoftPact: paramObject.SP == 1,
+            isSearchingMembers: paramObject.IS,
+            landmarks: [],
+            isOpenAlliance: paramObject.IA != 0,
+            freeRenames: paramObject.FR,
+            might: parseInt(paramObject.MP),
+        }
+        let memberListArray = paramObject.M;
+        let _memberList = [];
+        let i = 0;
+        while (i < memberListArray.length) {
+            if (memberListArray[i]?.OID === 75684) {
+                console.log("Macht4: " + require("../../data").players[75684].might);
             }
+            let member = wsp.parseOwnerInfo(memberListArray[i]);
+            if (member === null) continue;
+            if (member.playerId === 75684) {
+                console.log("Macht5: " + memberListArray[i].MP);
+                console.log("Macht6: " + member.might);
+            }
+            if (memberListArray[i] && memberListArray[i].OID) {
+                member = {
+                    playerId: memberListArray[i].OID,
+                    allianceRank: parseInt(memberListArray[i].AR),
+                }
+            }
+            if (member.playerId === 75684) {
+                console.log("Macht7: " + require("../../data").players[member.playerId].might);
+            }
+            if (member !== null)
+                _memberList.push(member);
+            i++;
         }
-        if(member != null)
-            _memberList.push(member);
-        i++;
-    }
-    _memberList.sort((a, b) => {
-        if (a.allianceRank < b.allianceRank) {
-            return -1;
-        }
-        if (a.allianceRank > b.allianceRank) {
-            return 1;
-        }
-        return 0;
-    });
+        _memberList.sort((a, b) => {
+            if (a.allianceRank < b.allianceRank) {
+                return -1;
+            }
+            if (a.allianceRank > b.allianceRank) {
+                return 1;
+            }
+            return 0;
+        });
 
-    let _memberIdList = [];
-    i = 0;
-    while (i < _memberList.length) {
-        _memberIdList.push(_memberList[i].playerId);
-        i++;
+        let _memberIdList = [];
+        i = 0;
+        while (i < _memberList.length) {
+            _memberIdList.push(_memberList[i].playerId);
+            i++;
+        }
+        allianceInfoVO.memberList = _memberIdList;
+        return allianceInfoVO;
     }
-    allianceInfoVO.memberList = _memberIdList;
-    return allianceInfoVO;
+    catch (e) {
+        Logger.logError(e);
+        return null;
+    }
 }
 
 /**
