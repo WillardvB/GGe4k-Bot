5b94a941e5d2803f2bc25b5c1206032bf1454a6a
vanBrusselGames <88274071+vanBrusselGames@users.noreply.github.com>
2022-08-23 12:23:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,10 +25,16 @@ client.login(process.env.dcToken);
 
 
 client.events = new Collection();
+client.slashCommands = new Collection();
 const eventFiles = fs.readdirSync(__dirname + '/events').filter(file => file.endsWith('.js'));
+const slashCommandFiles = fs.readdirSync(__dirname + '/dc_commands/_slash/commandUse').filter(file => file.endsWith('.js'));
 for (const file of eventFiles) {
 	const command = require(`./events/${file}`);
 	client.events.set(command.name, command);
+}
+for (const file of slashCommandFiles) {
+	const command = require(`./dc_commands/_slash/commandUse/${file}`);
+	client.slashCommands.set(command.name, command);
 }
 
 client.once('ready', () => {
